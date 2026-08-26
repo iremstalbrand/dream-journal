230363cc7be55a71bec25ba206ad6d65242177b0
Irem Stalbrand <iremstalbrand@gmail.com>
2026-08-26 23:38:09 +0200

    feat: add DreamDetail page and routing for individual dreams

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Dreams from './pages/Dreams'
 import NewDream from './pages/NewDream'
+import DreamDetail from './pages/DreamDetail'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<NewDream />} />
       <Route path="/dreams" element={<Dreams />} />
+      <Route path="/dream/:id" element={<DreamDetail />} />
     </Routes>
   )
 }
